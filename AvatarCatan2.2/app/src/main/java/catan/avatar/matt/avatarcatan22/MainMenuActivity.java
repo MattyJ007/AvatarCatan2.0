23e159ea54d49e5d1ab496f49635a0d6c1633b30
MattyJ007 <matt.jdickie007@gmail.com>
2016-11-08 21:13:11 +0200

    Copying Over code from old app

--- AvatarCatan2.2/app/src/main/java/catan/avatar/matt/avatarcatan22/MainMenuActivity.java
+++ AvatarCatan2.2/app/src/main/java/catan/avatar/matt/avatarcatan22/MainMenuActivity.java
@@ -1,7 +1,10 @@
 package catan.avatar.matt.avatarcatan22;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -9,5 +12,45 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        Button units = (Button) findViewById(R.id.button2);
+        Button rules = (Button) findViewById(R.id.button3);
+        Button newBattle = (Button) findViewById(R.id.button);
+        Button loadBattle = (Button) findViewById(R.id.button5);
+        Button timer = (Button) findViewById(R.id.button4);
+        units.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainMenuActivity.this, UnitsActivity.class);
+                startActivity(intent);
+            }
+        });
+        rules.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainMenuActivity.this, RulesActivity.class);
+                startActivity(intent);
+            }
+        });
+        newBattle.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainMenuActivity.this, OffensiveTeamChoiceActivity.class);
+                startActivity(intent);
+            }
+        });
+        loadBattle.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainMenuActivity.this, LoadBattleActivity.class);
+                startActivity(intent);
+            }
+        });
+        timer.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainMenuActivity.this, TimerActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
